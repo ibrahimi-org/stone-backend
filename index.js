c122aa41df12f6dba5649c29d0e0d5592e8b9b06
Besmillah Ibrahimi <besmillah@ibrahimi.info>
2024-10-15 23:45:17 +0330

    feat(): restful api

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ export const config = {
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   javascriptKey: process.env.JAVASCRIPT_KEY || '', //Add your master key here. Keep it secret!
-  // restApiKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  restAPIKey: process.env.REST_API_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
 
   // liveQuery: {
   //   classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   // },
   encodeParseObjectInCloudFunction: true,
+  verbose: false,
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
